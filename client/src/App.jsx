8f48831a9b3848db4e9cd2bb282ac714f12f4a9b
Sahil <sk12102003@gmail.com>
2026-06-11 17:11:58 +0530

    Added Tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 function App() {
-  return(
-  <div>
-    <h1>Expense Tracker</h1>
-  </div>
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Expense Tracker
+      </h1>
+    </div>
   );
 }
+
 export default App;
